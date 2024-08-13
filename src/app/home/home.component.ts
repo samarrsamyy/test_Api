c1dc0a8e5aa21e378687190d03a1440587ad1f45
Samar <samar8309@gmail.com>
2024-08-14 01:18:54 +0300

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { IProduct } from '../iproduct';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+
+  constructor( private _ProductsService:ProductsService){
+
+  }
+
+  products:IProduct[] =[]
+
+  ngOnInit(): void {
+    this._ProductsService.getProducts().subscribe({
+        next:(res)=>{
+        this.products=res
+        
+      },
+        error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+
+
+
+}
